7315757c13b50a5d71eb129f7d9463f326dbba5e
solomon2773 <solomon2773@yahoo.com>
2014-10-07 12:43:25 -0500

    v1.0.1

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 AIPantry.com
 
+We provide free online database for personal use via REST web service protocol.
 ========
---
+++ sample/sample.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by Solomon Tsao.
+ * Date: 10/7/14
+* V1.0
+ */
+    ///// Your API Key set, you can get it from your user account page. ///http://www.aipantry.com/user/default/0/
+    $api_name = '42Jeb42JebGSwx3mE047d';
+    $api_key = '4f991db8c901763344262c6a8ff477a9f598399f0cf152888755967c5d7e4cfabf16e8ac0a632ec0237e9f50f4ca2ac4552cafe4922426a6dc812a8a61401f23';
+
+
+    //// CREATE A TABLE
+
+    ///// RUNNING A QUERY
+
+
+
+
+
